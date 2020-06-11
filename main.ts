0539f0fa9eab5b6ca35d9a78e1807c29c3e7125f
Creepia <63631846+Creepia@users.noreply.github.com>
2020-06-11 21:43:13 +0800

    Update main.ts

--- main.ts
+++ main.ts
@@ -2,38 +2,57 @@
 // numbers here should be integer
 //% weight=0 color=#ADD8E6 block="Item"
 namespace Item {
-    //% blockId="autoCount" block="starttime = %starttime endtime = %endtime delay = %delay IsShown? %IsShown"
-    export function autoCount(starttime: number, endtime: number, delay: number = 1000, IsShown: boolean = true): void {
+    let globle_num: number
+    //shownWay should be 0(no animation),1(normal),2(with last-value-shown)
+    //% blockId="autoCount" block="starttime = %starttime endtime = %endtime delay = %delay shownWay %shownWay"
+    export function autoCount(starttime: number, endtime: number, delay: number = 1000, shownWay: number = 1): void {
         control.inBackground(function () {
             if (starttime <= endtime) {
                 for (let t = starttime; t <= endtime; t++) {
-                    if (IsShown) {
+                    if (shownWay != 0) {
                         basic.clearScreen()
                         lightNumberShow(t)
-                        basic.pause(delay)
                     }
+                    basic.pause(delay)
                 }
 
             }
             if (starttime > endtime) {
                 for (let u = starttime; u >= endtime; u--) {
-                    if (IsShown) {
+                    if (shownWay != 0) {
                         basic.clearScreen()
                         lightNumberShow(u)
-                        basic.pause(delay)
                     }
+                    basic.pause(delay)
                 }
 
+            }
+            if (shownWay == 2) {
+                basic.showNumber(globle_num)
             }
 
         })
     }
     function lightNumberShow(num: number) {
-        let x, y
-        for (let v = num - 1; v >= 0; v--) {
+        let x, y, v
+        let fullLights = 0
+        //Deal with variable num to between 0-25 and get the extra-25s to fullLights
+        for (; num < 0 || num > 25;) {
+            if (num < 0) {
+                num += 25
+                fullLights--
+            }
+            else {
+                num -= 25
+                fullLights++
+            }
+        }
+        //Show the light animation
+        for (v = num - 1; v >= 0; v--) {
             x = v % 5
             y = Math.floor(v / 5)
             led.plot(x, y)
         }
+        globle_num = num + fullLights * 25
     }
 }
